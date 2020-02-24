b64d79c530e15f948d7c9c1e02d8b2b97d188136
ArmHMM <armandnava@yahoo.com>
2020-02-23 21:42:50 -0600

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,237 +12,40 @@ User.create!(name: "Example User",
 	     password: "foobar",
 	     password_confirmation: "foobar",
 	     admin: true,
-         activated: true,
-         activated_at: Time.zone.now )
+	     activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name: "Pepito Perez",
 	     email: "pepitoperez@example.com",
 	     password: "pepito",
 	     password_confirmation: "pepito",
-         activated: true,
-         activated_at: Time.zone.now)
+	     activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name: "Juan Perez",
 	     email: "juanperez@example.com",
 	     password: "juanito",
 	     password_confirmation: "juanito",
-         activated: true,
-         activated_at: Time.zone.now)
-
-User.create!(name: "r1",
-	     email: "r1@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-         activated: true,
-         activated_at: Time.zone.now)
-
-User.create!(name: "r2",
-	     email: "r2@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
+	     activated: true,
              activated_at: Time.zone.now)
 
-User.create!(name: "r3",
-	     email: "r3@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r4",
-	     email: "r4@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r5",
-	     email: "r5@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r6",
-	     email: "r6@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r7",
-	     email: "r7@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r8",
-	     email: "r8@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r9",
-	     email: "r9@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r10",
-	     email: "r10@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r11",
-	     email: "r11@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r12",
-	     email: "r12@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r13",
-	     email: "r13@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r14",
-	     email: "r14@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r15",
-	     email: "r15@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789")
-
-User.create!(name: "r16",
-	     email: "r16@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789")
-
-User.create!(name: "r17",
-	     email: "r17@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789")
-
-#User.create!(name: "r17",#
-	     #email: "r17@example.com",
-	     #password: "123456789",
-	     #password_confirmation: "123456789")
-
-User.create!(name: "r18",
-	     email: "r18@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r19",
-	     email: "r19@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r20",
-	     email: "r20@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r21",
-	     email: "r21@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "r22",
-	     email: "r22@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r23",
-	     email: "r23@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r24",
-	     email: "r24@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r25",
-	     email: "r25@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r26",
-	     email: "r26@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r27",
-	     email: "r27@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-User.create!(name: "r28",
-	     email: "r28@example.com",
-	     password: "123456789",
-	     password_confirmation: "123456789",
-             activated: true,
-             activated_at: Time.zone.now)
-
-# ... (-_-¡)
+99.times do |n|
+	name = Faker::Name.name
+	email = "example-#{n+1}@railstutorial.org"
+	password = "password",
+	User.create!(name: name,
+		    email: email,
+		    password: password,
+		    password_confirmation: password,
+		    activated: true,
+	            activated_at: Time.zone.now)
+end
 
 users = User.order(:created_at).take(6)
 50.times do
 	content = Faker::Lorem.sentence(5)
 	users.each{|user| user.microposts.create!(content: content)}
 end
-
 
 # Following relationships
 users = User.all
